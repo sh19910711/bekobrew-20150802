1b1d2de985f300742b91b248243b7d59b6c4d19a
Hiroyuki Sano <sh19910711@gmail.com>
2014-06-14 08:25:11 +0900

    edit makepkg.sh

--- src/bekobrew/makepkg.sh
+++ src/bekobrew/makepkg.sh
@@ -1,2 +1,34 @@
-echo makepkg
-echo $@
+current_dir=`pwd`
+source_dir=${current_dir}/src
+package_dir=${current_dir}/pkg
+
+shopt -u extglob
+source BKGBUILD
+shopt -s extglob
+
+rm -rf ${package_dir} || true
+
+# create source dir
+mkdir -p "${source_dir}"
+chmod a-s "${source_dir}"
+
+cd ${source_dir}
+
+# download
+for url in $source; do
+  wget -c $url
+  [[ "$url" =~ .*\/(.+)$ ]]
+  filename=${BASH_REMATCH[1]}
+  tar xvf ${filename}
+done
+cd ${current_dir}
+
+cd ${current_dir}
+
+# packaging
+build
+
+cd ${current_dir}
+
+package
+
