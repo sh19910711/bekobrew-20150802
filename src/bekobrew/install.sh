ef681035fcbc220ed7660420a15939b327d8f4c9
Hiroyuki Sano <sh19910711@gmail.com>
2014-06-19 17:16:19 +0900

    bekobrew-install: resolve dependencies

--- src/bekobrew/install.sh
+++ src/bekobrew/install.sh
@@ -3,12 +3,31 @@
 
 function install_package() {
   local tmpdir=`mktemp -d /tmp/bekobrew-XXXXXX`
+  local package_name=''
+  local package_version=''
+  local package_release=''
+
   pushd ${tmpdir}
   bekobrew get $1
   cd $1
   source BEKOBUILD
-  bekobrew makepkg
+
+  local package_fullname=${package_name}-${package_version}-${package_release}
+
+  # インストール済みであれば何もしない
+  grep ${package_fullname} ~/.bekobrew/installed_packages
+  local ret=$?
+  if [ ${ret} -ne 0 ]; then
+    # 依存するパッケージの検索
+    for dep in ${package_depends[@]}; do
+      installed_package ${dep}
+    done
+    bekobrew makepkg
+    echo ${package_fullname} > ~/.bekobrew/installed_packages
+  fi
+
   popd
+  rm -rf ${tmpdir}
 }
 
 install_package $@
